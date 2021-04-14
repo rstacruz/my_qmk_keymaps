2d7e2c2512e6445e411e750a8b7f8a0968625202
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2021-04-14 11:36:57 +1000

    Update

--- qmk-planck/keymap.c
+++ qmk-planck/keymap.c
@@ -4,17 +4,20 @@
 #define ___  KC_NO
 
 /* Modifier keys */
-#define xx_LCMD KC_LALT
-#define xx_LSFT KC_LSFT
-#define xx_LOPT KC_LGUI
-#define xx_SYM  MO(_SYM)
-#define xx_NAV  MO(_NAV)
-#define xx_ENT  LCTL_T(KC_ENT)
-#define xx_Q    LCTL_T(KC_Q)
+#define x__LCMD KC_LALT
+#define x__LSFT KC_LSFT
+#define x__LOPT KC_LGUI
+#define x__OTAB MT(MOD_LGUI,KC_TAB) /* opt/gui | tab */
+#define x__CESC MT(MOD_LALT,KC_ESC) /* cmd/alt | esc */
+#define x__SYM  MO(_SYM)
+#define x__NAV  MO(_NAV)
+#define x__ENT  LCTL_T(KC_ENT)  /* ctrl | enter */
+#define x__Q    LCTL_T(KC_Q)    /* ctrl | q */
+#define x__CBSP C(KC_BSPC) /* Delete word */
 
 /* Tab variations */
-#define xx_CTAB RCTL(KC_TAB)
-#define xx_STAB RCTL(LSFT(KC_TAB))
+#define x__CTAB RCTL(KC_TAB)
+#define x__STAB RCTL(LSFT(KC_TAB))
 
 /* Layout */
 #define LAYOUT_36( \
@@ -31,7 +34,7 @@
   )
 
 /* Layers */
-enum layers { _BASE = 0, _DHR, _SYM, _NAV, _FUN, _PAD, _GAM, _GMX, _LOC };
+enum layers { _BASE = 0, _ALT, _SYM, _NAV, _FUN, _PAD, _GAM, _GMX, _LOC };
 
 /* Macros */
 enum custom_keycodes {
@@ -54,20 +57,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = { /*
                       └────┴────┴───────┴───────┴────┴────┘ */
 
   [0] = LAYOUT_36(
-    xx_Q, KC_W, KC_F,    KC_P,   KC_B,    /**/ KC_J,   KC_L,   KC_U,    KC_Y,   KC_BSPC,
+    x__Q, KC_W, KC_F,    KC_P,   KC_B,    /**/ KC_J,   KC_L,   KC_U,    KC_Y,   KC_BSPC,
     KC_A, KC_R, KC_S,    KC_T,   KC_G,    /**/ KC_M,   KC_N,   KC_E,    KC_I,   KC_O,
-    KC_Z, KC_X, KC_C,    KC_D,   KC_V,    /**/ KC_K,   KC_H,   KC_COMM, KC_DOT, xx_ENT,
-    /**/  /**/  xx_LCMD, xx_SYM, xx_LSFT, /**/ KC_SPC, xx_NAV, xx_LOPT  /**/    /**/
+    KC_Z, KC_X, KC_C,    KC_D,   KC_V,    /**/ KC_K,   KC_H,   KC_COMM, KC_DOT, x__ENT,
+    /**/  /**/  x__CESC, x__SYM, x__LSFT, /**/ KC_SPC, x__NAV, x__OTAB  /**/    /**/
   ), /*
 
   ┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄
-  Alternate layer*/
+  Alternate layer */
 
-  [_DHR] = LAYOUT_36(
-    _v_, _v_, _v_,                 _v_, _v_, /**/ _v_, _v_, _v_,                _v_, _v_,
-    _v_, _v_, _v_,                 _v_, _v_, /**/ _v_, _v_, _v_,                _v_, _v_,
-    _v_, _v_, _v_,                 _v_, _v_, /**/ _v_, _v_, _v_,                _v_, _v_,
-    /**/ /**/ MT(MOD_LALT,KC_ESC), _v_, _v_, /**/ _v_, _v_, MT(MOD_LGUI,KC_TAB) /**/ /**/
+  [_ALT] = LAYOUT_36(
+    _v_, _v_, _v_,     _v_, _v_, /**/ _v_, _v_, _v_,    _v_, _v_,
+    _v_, _v_, _v_,     _v_, _v_, /**/ _v_, _v_, _v_,    _v_, _v_,
+    _v_, _v_, _v_,     _v_, _v_, /**/ _v_, _v_, _v_,    _v_, _v_,
+    /**/ /**/ x__LCMD, _v_, _v_, /**/ _v_, _v_, x__LOPT /**/ /**/
   ), /*
 
   ┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄
@@ -92,18 +95,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = { /*
   Navigate  ┌────┬────┐────┬────┬────┐     ┌────┬────┏━━━━┓────┬────┐
             │ ⌃  │ ⌥  │ ↹  │    │    │     │pg↑ │home┃ ▲  ┃end │ ^⌫ │
             └────┴────┘────┼────┼────┤     ├────┏━━━━┛────┗━━━━┓────┤
-            │ 1  │ 2  │ 3  │ 4  │ 5  │     │pg↓ ┃ ◀  │ ▼  │ ▶  ┃ ↵  │
+            │ 1  │ 2  │ 3  │ 4  │ 5  │     │pg↓ ┃ ◀  │ ▼  │ ▶  ┃    │
   ┌┄┄┄┄┐    ├────┼────┼────┼────┼────┤     ├────┗━━━━┷━━━━┷━━━━┛────┤
-  ┆NAV ┆ ┄› │ 6  │ 7  │ 8  │ 9  │ 0  │     │ ⎋  │    │    │ .  │ ⌦  │
+  ┆NAV ┆ ┄› │ 6  │ 7  │ 8  │ 9  │ 0  │     │ ⎋  │ ↵  │    │ .  │ ⌦  │
   └┄┄┄┄┘    └────┴────┴────┴────┴────┘──┬──└────┴────┴────┴────┴────┘
-                      │    │PAD │       │   ↵   │ ▓▓ │    │
+                      │    │PAD │       │       │ ▓▓ │    │
                       └────┴────┴───────┴───────┴────┴────┘ */
 
   [_NAV] = LAYOUT_36(
-    KC_LCTL, KC_LGUI, KC_TAB, KC_ESC,   C(KC_BSPC), /**/ KC_PGUP, KC_HOME,  KC_UP,   KC_END,  C(KC_BSPC),
-    KC_1,    KC_2,    KC_3,   KC_4,     KC_5,       /**/ KC_PGDN, KC_LEFT,  KC_DOWN, KC_RGHT, KC_ENT,
-    KC_6,    KC_7,    KC_8,   KC_9,     KC_0,       /**/ KC_ESC,  KC_ENT,   KC_BSPC, KC_DOT,  KC_DEL,
-    /**/     /**/     _v_,    MO(_PAD), _v_,        /**/ KC_ENT,  TG(_NAV), _v_      /**/     /**/
+    KC_LCTL, KC_LGUI, KC_TAB, KC_ESC,   KC_ESC, /**/ KC_PGUP, KC_HOME,  KC_UP,   KC_END,  x__CBSP,
+    KC_1,    KC_2,    KC_3,   KC_4,     KC_5,   /**/ KC_PGDN, KC_LEFT,  KC_DOWN, KC_RGHT, KC_ENT,
+    KC_6,    KC_7,    KC_8,   KC_9,     KC_0,   /**/ KC_ESC,  KC_ENT,   KC_ENT,  KC_DOT,  KC_DEL,
+    /**/     /**/     _v_,    MO(_PAD), _v_,    /**/ _v_,     TG(_NAV), _v_      /**/     /**/
   ), /*
 
   ┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄
@@ -118,7 +121,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = { /*
                       └────┴────┴───────┴───────┴────┴────┘ */
 
   [_FUN] = LAYOUT_36(
-    KC_F11, KC_F12, KC_MS_BTN2, KC_MS_BTN1, KC_MS_BTN3, /**/ KC_WH_U, xx_STAB,  KC_MS_U, xx_CTAB, KC_BRIU,
+    KC_F11, KC_F12, KC_MS_BTN2, KC_MS_BTN1, KC_MS_BTN3, /**/ KC_WH_U, x__STAB,  KC_MS_U, x__CTAB, KC_BRIU,
     KC_F1,  KC_F2,  KC_F3,      KC_F4,      KC_F5,      /**/ KC_WH_D, KC_MS_L,  KC_MS_D, KC_MS_R, KC_BRID,
     KC_F6,  KC_F7,  KC_F8,      KC_F9,      KC_F10,     /**/ KC_VOLD, KC_VOLU,  KC_MPLY, KC_MNXT, RESET,
     /**/    /**/    _v_,        KC_MS_BTN1, _v_,        /**/ _v_,     TG(_FUN), _v_      /**/     /**/
@@ -136,7 +139,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = { /*
                       └────┴────┴───────┴───────┴────┴────┘ */
 
   [_PAD] = LAYOUT_36(
-    TG(_DHR), ___,     ___,     ___,         ___, /**/ MC_MUTE, MC_HAND, ___,    ___,    ___,
+    TG(_ALT), ___,     ___,     ___,         ___, /**/ MC_MUTE, MC_HAND, ___,    ___,    ___,
     DF(_GAM), ___,     ___,     ___,         ___, /**/ MC_SHOT, KC_F13,  KC_F14, KC_F15, KC_F16,
     DF(_LOC), CMB_TOG, KC_CAPS, KC_WWW_HOME, ___, /**/ ___,     KC_F17,  KC_F18, KC_F19, KC_F20,
     /**/      /**/     _v_,     _v_,         _v_, /**/ _v_,     _v_,     _v_     /**/    /**/
@@ -193,34 +196,29 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = { /*
 }; /*
 
 ┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄
-Chords      ┌────┬────┬────┬────┬────┐     ┌────┬────┬────┬────┬────┐
-            │    │    │    │    │    │     │    │    │    │ y  │ bs │
-            └────┴────┴────┴────┴────┘     └────┴────┴────┴────┴────┘
-                                                 ┌  ↵  ┄┐
+                                                  ┌┄┄┄┄┄ ⎋ ┄┐
+Chords                                      ┌┄⎋ ┄┐┌┄↵  ┐
             ┌────┬────┬────┬────┬────┐     ┌────┬────┬────┬────┬────┐
             │    │    │    │    │    │     │ k  │ h  │ ,  │ .  │    │
-            └────┴────┴────┴────┴────┴─────┴────┴────┴────┴────┴────┘
-Chords      ┌────┬────┬────┬────┬────┐     ┌────┬────┬────┬────┬────┐
-with space  │    │    │    │    │    │     │ ⎋  │    │    │ ⌫  │    │
             └────┴────┴────┴────┴────┴─────┴────┴────┴────┴────┴────┘
 */
 
 enum combos { CM_0, CM_1, CM_2, CM_3, CM_4, CM_5, CM_6, CM_7, CM_8, CM_9, CM_10, CM_11, CM_12 };
 
 const uint16_t PROGMEM combo_0[] = {KC_H, KC_COMM, COMBO_END};
-const uint16_t PROGMEM combo_1[] = {KC_SPC, KC_K, COMBO_END};
-const uint16_t PROGMEM combo_2[] = {KC_SPC, KC_DOT, COMBO_END};
+const uint16_t PROGMEM combo_1[] = {KC_H, KC_K, COMBO_END};
+const uint16_t PROGMEM combo_2[] = {KC_H, KC_DOT, COMBO_END};
 
 combo_t key_combos[COMBO_COUNT] = {
   [CM_0] = COMBO(combo_0, KC_ENT),
   [CM_1] = COMBO(combo_1, KC_ESC),
-  [CM_2] = COMBO(combo_2, C(KC_BSPC)),
+  [CM_2] = COMBO(combo_2, x__CBSP),
 };
 
 // https://beta.docs.qmk.fm/using-qmk/software-features/tap_hold#ignore-mod-tap-interrupt
 bool get_ignore_mod_tap_interrupt(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
-    case xx_Q:
+    case x__Q:
       return true;
     default:
       return false;
@@ -229,7 +227,7 @@ bool get_ignore_mod_tap_interrupt(uint16_t keycode, keyrecord_t *record) {
 
 uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
-    case xx_Q:
+    case x__Q:
       return TAPPING_TERM_LONG;
     default:
       return TAPPING_TERM;
