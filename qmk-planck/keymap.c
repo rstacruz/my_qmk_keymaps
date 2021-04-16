6e0d938b347175690b0a2d6dae53560b8dc916d4
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2021-04-16 14:16:54 +1000

    Add macros

--- qmk-planck/keymap.c
+++ qmk-planck/keymap.c
@@ -103,10 +103,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = { /*
                       └────┴────┴───────┴───────┴────┴────┘ */
 
   [_NAV] = LAYOUT_36(
-    KC_LCTL, KC_LGUI, KC_TAB, KC_ESC,   KC_ESC, /**/ KC_PGUP, KC_HOME,  KC_UP,   KC_END,  x__CBSP,
-    KC_1,    KC_2,    KC_3,   KC_4,     KC_5,   /**/ KC_PGDN, KC_LEFT,  KC_DOWN, KC_RGHT, KC_ENT,
-    KC_6,    KC_7,    KC_8,   KC_9,     KC_0,   /**/ KC_ESC,  KC_ENT,   KC_ENT,  KC_DOT,  KC_DEL,
-    /**/     /**/     _v_,    MO(_PAD), _v_,    /**/ _v_,     TG(_NAV), _v_      /**/     /**/
+    KC_LCTL, KC_LGUI, KC_TAB, MC_HAND,  MC_MUTE, /**/ KC_PGUP, KC_HOME,  KC_UP,   KC_END,  x__CBSP,
+    KC_1,    KC_2,    KC_3,   KC_4,     KC_5,    /**/ KC_PGDN, KC_LEFT,  KC_DOWN, KC_RGHT, KC_ENT,
+    KC_6,    KC_7,    KC_8,   KC_9,     KC_0,    /**/ KC_ESC,  KC_ENT,   KC_ENT,  KC_DOT,  KC_DEL,
+    /**/     /**/     _v_,    MO(_PAD), _v_,     /**/ _v_,     TG(_NAV), _v_      /**/     /**/
   ), /*
 
   ┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄
