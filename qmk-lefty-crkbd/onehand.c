d15d9f54d77dcbab2a22f2e39361ba414ee72a68
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2021-06-29 16:36:24 +1000

    Update one hand

--- qmk-lefty-crkbd/onehand.c
+++ qmk-lefty-crkbd/onehand.c
@@ -40,32 +40,41 @@ DEF_COMBO_INPUT_KC(PGUP, UP);
 DEF_COMBO_INPUT_KC(UP, PGDN);
 
 combo_t key_combos[COMBO_COUNT] = {
+  /* Actions */
   COMBO_ACTION(combo_N_A),            /* . . . n | a . . . - shift */
   COMBO_ACTION(combo_O_N),            /* o . . n | . . . . - mod */
   COMBO_ACTION(combo_N_T),            /* . . . n | . . . t - nav */
   COMBO_ACTION(combo_O_A),            /* o . . . | a . . . - num */
+  /* Adjacents */
   DEF_COMBO_TARGET_KC(O, I, F),       /* o i . . | . . . . */
-  DEF_COMBO_TARGET_KC(O, E, Z),       /* o . e . | . . . . */
-  DEF_COMBO_TARGET_KC(O, R, X),       /* o . . . | . r . . */
-  DEF_COMBO_TARGET_KC(O, S, V),       /* o . . . | . . s . */
-  DEF_COMBO_TARGET_KC(O, T, BSPC),    /* o . . . | . . . t */
   DEF_COMBO_TARGET_KC(I, E, C),       /* . i e . | . . . . */
-  DEF_COMBO_TARGET_KC(I, N, J),       /* . i . n | . . . . */
-  DEF_COMBO_TARGET_KC(I, A, B),       /* . i . . | a . . . */
-  DEF_COMBO_TARGET_KC(I, S, M),       /* . i . . | . . s . */
-  DEF_COMBO_TARGET_KC(I, T, P),       /* . i . . | . . . t */
   DEF_COMBO_TARGET_KC(E, N, U),       /* . . e n | . . . . */
-  DEF_COMBO_TARGET_KC(E, A, QUOT),    /* . . e . | a . . . */
-  DEF_COMBO_TARGET_KC(E, R, G),       /* . . e . | . r . . */
-  DEF_COMBO_TARGET_KC(E, T, L),       /* . . e . | . . . t */
-  DEF_COMBO_TARGET_KC(N, R, DOT),     /* . . . n | . r . . */
-  DEF_COMBO_TARGET_KC(N, S, Y),       /* . . . n | . . s . */
   DEF_COMBO_TARGET_KC(A, R, W),       /* . . . . | a r . . */
-  DEF_COMBO_TARGET_KC(A, S, Q),       /* . . . . | a . s . */
-  DEF_COMBO_TARGET_KC(A, T, SPC),     /* . . . . | a . . t */
   DEF_COMBO_TARGET_KC(R, S, D),       /* . . . . | . r s . */
-  DEF_COMBO_TARGET_KC(R, T, K),       /* . . . . | . r . t */
   DEF_COMBO_TARGET_KC(S, T, H),       /* . . . . | . . s t */
+  /* Triplets */
+  DEF_COMBO_TARGET_KC(O, E, G),       /* o . e . | . . . . */
+  DEF_COMBO_TARGET_KC(I, N, M),       /* . i . n | . . . . */
+  DEF_COMBO_TARGET_KC(A, S, V),       /* . . . . | a . s . */
+  DEF_COMBO_TARGET_KC(R, T, Y),       /* . . . . | . r . t */
+  /* Knights */
+  DEF_COMBO_TARGET_KC(O, S, V),       /* o . . . | . . s . */
+  DEF_COMBO_TARGET_KC(I, T, P),       /* . i . . | . . . t */
+  DEF_COMBO_TARGET_KC(E, A, QUOT),    /* . . e . | a . . . */
+  DEF_COMBO_TARGET_KC(N, R, DOT),     /* . . . n | . r . . */
+  /* Four-wides */
+  DEF_COMBO_TARGET_KC(A, T, SPC),     /* . . . . | a . . t */
+  /* Diagonals, upward */
+  DEF_COMBO_TARGET_KC(I, A, B),       /* . i . . | a . . . */
+  DEF_COMBO_TARGET_KC(E, R, Z),       /* . . e . | . r . . */
+  DEF_COMBO_TARGET_KC(N, S, J),       /* . . . n | . . s . */
+  /* Diagonals, downward */
+  DEF_COMBO_TARGET_KC(O, R, X),       /* o . . . | . r . . */
+  DEF_COMBO_TARGET_KC(I, S, Q),       /* . i . . | . . s . */
+  DEF_COMBO_TARGET_KC(E, T, L),       /* . . e . | . . . t */
+  /* Wide diagonals */
+  DEF_COMBO_TARGET_KC(O, T, BSPC),    /* o . . . | . . . t */
+  /* Navs */
   DEF_COMBO_TARGET_KC(ENT, TAB, ESC),
   DEF_COMBO_TARGET_KC(PGUP, UP, HOME),
   DEF_COMBO_TARGET_KC(UP, PGDN, END),
@@ -74,8 +83,38 @@ combo_t key_combos[COMBO_COUNT] = {
 void keyboard_post_init_user(void) {
   // Disable combos on startup
   combo_disable();
+
+	// Enable the LED layers
+	layer_state_set_user(layer_state);
 }
 #endif
+
+extern rgblight_config_t rgblight_config;
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+  switch(get_highest_layer(state)) {
+  case _OH_BAS:
+    rgblight_enable_noeeprom();
+    rgblight_sethsv_noeeprom(HSV_WHITE);
+    break;
+  case _OH_MOD:
+    rgblight_enable_noeeprom();
+    rgblight_sethsv_noeeprom(HSV_PURPLE);
+    break;
+  case _OH_NAV:
+    rgblight_enable_noeeprom();
+    rgblight_sethsv_noeeprom(HSV_TEAL);
+    break;
+  case _OH_NUM:
+    rgblight_enable_noeeprom();
+    rgblight_sethsv_noeeprom(HSV_GREEN);
+    break;
+  default:
+    rgblight_disable_noeeprom();
+    break;
+}
+return state;
+}
 
 bool onehand_process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
@@ -83,8 +122,8 @@ bool onehand_process_record_user(uint16_t keycode, keyrecord_t *record) {
       if (record->event.pressed) {
         layer_on(_OH_BAS);
         combo_enable();
-        rgblight_enable_noeeprom();
-        rgblight_sethsv_noeeprom(HSV_PURPLE);
+        /* rgblight_enable_noeeprom(); */
+        /* rgblight_sethsv_noeeprom(HSV_PURPLE); */
       }
       return false;
       break;
@@ -95,7 +134,7 @@ bool onehand_process_record_user(uint16_t keycode, keyrecord_t *record) {
         layer_off(_OH_NAV);
         layer_off(_OH_NUM);
         combo_disable();
-        rgblight_disable_noeeprom();
+        /* rgblight_disable_noeeprom(); */
       }
       return false;
       break;
