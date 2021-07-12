3a8a80b01dbba5eaeebb4967e6746ecfa0358e9f
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2021-07-12 23:19:52 +1000

    Add game mode back

--- qmk-lefty-crkbd/config.h
+++ qmk-lefty-crkbd/config.h
@@ -22,8 +22,8 @@
 #define MOUSEKEY_DELAY          0
 #define MOUSEKEY_INTERVAL       16
 #define MOUSEKEY_WHEEL_DELAY    0
-#define MOUSEKEY_MAX_SPEED      6
-#define MOUSEKEY_TIME_TO_MAX    32
+#define MOUSEKEY_MAX_SPEED      12
+#define MOUSEKEY_TIME_TO_MAX    16
 
 /* Audio */
 #ifdef AUDIO_ENABLE
